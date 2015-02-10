de0c9f6329eb2c419f0b8da766c702967c113921
Shaun Taheri <st@optiopay.com>
2015-02-10 16:46:31 +0100

    rename KafkaError to APIError to avoid package name conflict

--- api.go
+++ api.go
@@ -22,38 +22,37 @@ const (
 )
 
 var apiErrors = map[int16]error{
-	-1: &KafkaError{-1, "unknown"},
-	0:  &KafkaError{0, "no error"},
-	1:  &KafkaError{1, "offset out of range"},
-	2:  &KafkaError{2, "invalid message"},
-	3:  &KafkaError{3, "unknown topic or partition"},
-	4:  &KafkaError{4, "invalid message size"},
-	5:  &KafkaError{5, "leader not available"},
-	6:  &KafkaError{6, "no leader for partition"},
-	7:  &KafkaError{7, "request timeed out"},
-	8:  &KafkaError{8, "broker not available"},
-	9:  &KafkaError{9, "replica not available"},
-	10: &KafkaError{10, "message size too large"},
-	11: &KafkaError{11, "scale controller epoch"},
-	12: &KafkaError{12, "ofset metadata too large"},
-	14: &KafkaError{14, "offsets load in progress"},
-	15: &KafkaError{15, "consumer coordinator not available"},
-	16: &KafkaError{16, "not coordinator for consumer"},
+	-1: &APIError{-1, "unknown"},
+	0:  &APIError{0, "no error"},
+	1:  &APIError{1, "offset out of range"},
+	2:  &APIError{2, "invalid message"},
+	3:  &APIError{3, "unknown topic or partition"},
+	4:  &APIError{4, "invalid message size"},
+	5:  &APIError{5, "leader not available"},
+	6:  &APIError{6, "no leader for partition"},
+	7:  &APIError{7, "request timeed out"},
+	8:  &APIError{8, "broker not available"},
+	9:  &APIError{9, "replica not available"},
+	10: &APIError{10, "message size too large"},
+	11: &APIError{11, "scale controller epoch"},
+	12: &APIError{12, "ofset metadata too large"},
+	14: &APIError{14, "offsets load in progress"},
+	15: &APIError{15, "consumer coordinator not available"},
+	16: &APIError{16, "not coordinator for consumer"},
 }
 
-type KafkaError struct {
+// APIError represents an error from the Kafka API.
+type APIError struct {
 	errno   int16
 	message string
 }
 
-func (err *KafkaError) Error() string {
+// Error returns a printable error string.
+func (err *APIError) Error() string {
 	return fmt.Sprintf("%s (%d)", err.message, err.errno)
 }
 
-func (err *KafkaError) Errno() int {
-	return int(err.errno)
-}
-
+// errFromNo returns the error for a specific errno.
 func errFromNo(errno int16) error {
 	if errno == 0 {
 		return nil
--- fetch.go
+++ fetch.go
@@ -3,6 +3,8 @@ package kafka
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
+	"hash/crc32"
 	"io"
 	"time"
 )
@@ -48,6 +50,34 @@ type FetchRespPartition struct {
 	Err       error
 	TipOffset int64
 	Messages  []*Message
+}
+
+// Message encapsualtes a Kafka message.
+type Message struct {
+	Offset int64
+	Crc    uint32
+	Key    []byte
+	Value  []byte
+}
+
+// Bytes encodes a Message to a list of bytes.
+func (m *Message) Bytes() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := newEncoder(&buf)
+
+	enc.Encode(int32(0)) // crc placeholder
+	enc.Encode(int8(0))  // magic byte is always 0
+	enc.Encode(int8(0))  // no compress support
+	enc.Encode(m.Key)
+	enc.Encode(m.Value)
+
+	if enc.Err() != nil {
+		return nil, enc.Err()
+	}
+
+	b := buf.Bytes()
+	binary.BigEndian.PutUint32(b, crc32.ChecksumIEEE(b[4:])) // update crc
+	return b, nil
 }
 
 // Bytes coverts a FetchReq object to a list of bytes.
@@ -129,4 +159,39 @@ func ReadFetchResp(r io.Reader) (*FetchResp, error) {
 	}
 
 	return &resp, nil
+}
+
+// readMessageSet read in each Kafka message until EOF.
+func readMessageSet(r io.Reader) ([]*Message, error) {
+	set := make([]*Message, 0, 32)
+	dec := newDecoder(r)
+	msg := &Message{}
+
+	var offset int64
+	var attributes int8
+	var err error
+
+	for {
+		offset = dec.DecodeInt64()
+		if err = dec.Err(); err != nil {
+			if err == io.EOF {
+				return set, nil
+			}
+			return nil, err
+		}
+
+		_ = dec.DecodeInt32() // single message size
+		msg.Offset = offset
+		msg.Crc = dec.DecodeUint32() // TODO(husio) check crc
+		_ = dec.DecodeInt8()         // magic byte
+
+		attributes = dec.DecodeInt8()
+		if attributes != compressNone {
+			return nil, errors.New("cannot read compressed message") // TODO(husio)
+		}
+
+		msg.Key = dec.DecodeBytes()
+		msg.Value = dec.DecodeBytes()
+		set = append(set, msg)
+	}
 }
