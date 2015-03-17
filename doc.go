75f1d9f0c7e6ba05febbae38b84cdf1c8d085314
Piotr Husiatyński <phusiatynski@gmail.com>
2015-03-17 15:43:26 +0100

    interfaces renaming cleanup

--- doc.go
+++ doc.go
@@ -3,8 +3,8 @@
 Package kafka provides high level client for Apache Kafka.
 
 Use Broker for node connection management, Producer for sending messages and
-Consumer for fetching. All those structures implement Client, Fetcher and
-Sender interface, that is also implemented in kafkatest package.
+Consumer for fetching. All those structures implement Client, Consumer and
+Producer interface, that is also implemented in kafkatest package.
 
 */
 package kafka
