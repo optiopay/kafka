602e115e15c6e9029ff47300859b25865b236064
Piotr Husiatyński <phusiatynski@gmail.com>
2015-03-23 09:49:16 +0100

    example code updated

--- example_test.go
+++ example_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/optiopay/kafka/proto"
 )
 
-func ExampleConsumer() {
+func ExampleClient_Consumer() {
+	// connect to kafka cluster
 	addresses := []string{"localhost:9092", "localhost:9093"}
 	broker, err := Dial(addresses, NewBrokerConf("test"))
 	if err != nil {
@@ -16,16 +17,15 @@ func ExampleConsumer() {
 	}
 	defer broker.Close()
 
+	// create new consumer
 	conf := NewConsumerConf("my-messages", 0)
-	conf.RetryLimit = 20
-	conf.Log = log.New(os.Stderr, "[consumer:my-messages]", log.LstdFlags)
 	conf.StartOffset = StartOffsetNewest
-
 	consumer, err := broker.Consumer(conf)
 	if err != nil {
 		panic(err)
 	}
 
+	// read all messages
 	for {
 		msg, err := consumer.Consume()
 		if err != nil {
@@ -39,7 +39,8 @@ func ExampleConsumer() {
 	}
 }
 
-func ExampleProducer() {
+func ExampleClient_OffsetCoordinator() {
+	// connect to kafka cluster
 	addresses := []string{"localhost:9092", "localhost:9093"}
 	broker, err := Dial(addresses, NewBrokerConf("test"))
 	if err != nil {
@@ -47,10 +48,45 @@ func ExampleProducer() {
 	}
 	defer broker.Close()
 
+	// create offset coordinator and customize configuration
+	conf := NewOffsetCoordinatorConf("my-consumer-group")
+	conf.Log = log.New(os.Stderr, "[my-group-coordinator]", log.LstdFlags)
+	coordinator, err := broker.OffsetCoordinator(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// write consumed message offset for topic/partition
+	if err := coordinator.Commit("my-topic", 0, 12); err != nil {
+		panic(err)
+	}
+
+	// get latest consumed offset for given topic/partition
+	off, _, err := coordinator.Offset("my-topic", 0)
+	if err != nil {
+		panic(err)
+	}
+
+	if off != 12 {
+		panic(fmt.Sprintf("offset is %d, not 12", off))
+	}
+}
+
+func ExampleClient_Producer() {
+	// connect to kafka cluster
+	addresses := []string{"localhost:9092", "localhost:9093"}
+	broker, err := Dial(addresses, NewBrokerConf("test"))
+	if err != nil {
+		panic(err)
+	}
+	defer broker.Close()
+
+	// create new producer
 	conf := NewProducerConf()
 	conf.Log = log.New(os.Stderr, "[producer]", log.LstdFlags)
 	conf.RequiredAcks = proto.RequiredAcksLocal
 
+	// write two messages to kafka using single call to make it atomic
 	producer := broker.Producer(conf)
 	messages := []*proto.Message{
 		&proto.Message{Value: []byte("first")},
@@ -61,7 +97,8 @@ func ExampleProducer() {
 	}
 }
 
-func ExampleMultiplexer() {
+func ExampleMerge() {
+	// connect to kafka cluster
 	addresses := []string{"localhost:9092", "localhost:9093"}
 	broker, err := Dial(addresses, NewBrokerConf("test"))
 	if err != nil {
@@ -72,6 +109,7 @@ func ExampleMultiplexer() {
 	topics := []string{"fruits", "vegetables"}
 	fetchers := make([]Consumer, len(topics))
 
+	// create consumers for different topics
 	for i, topic := range topics {
 		conf := NewConsumerConf(topic, 0)
 		conf.RetryLimit = 20
@@ -84,9 +122,11 @@ func ExampleMultiplexer() {
 		fetchers[i] = consumer
 	}
 
+	// merge all created consumers
 	mx := Merge(fetchers...)
 	defer mx.Close()
 
+	// consume messages from all sources
 	for {
 		msg, err := mx.Consume()
 		if err != nil {
