46172a386fd00224b13d971af3564bf380ba01cc
Piotr Husiatyński <phusiatynski@gmail.com>
2015-07-15 08:51:01 +0200

    logging interface adjusted

--- log.go
+++ log.go
@@ -1,16 +1,22 @@
 package kafka
 
+// Logger is general logging interface that can be provided by popular logging
+// frameworks.
+//
+// * https://github.com/go-kit/kit/tree/master/log
+// * https://github.com/husio/log
 type Logger interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
+	Debug(msg string, args ...interface{})
+	Info(msg string, args ...interface{})
+	Warn(msg string, args ...interface{})
+	Error(msg string, args ...interface{})
 }
 
+// nullLogger implements Logger interface, but discards all messages
 type nullLogger struct {
 }
 
-func (nullLogger) Debug(args ...interface{}) {}
-func (nullLogger) Info(args ...interface{})  {}
-func (nullLogger) Warn(args ...interface{})  {}
-func (nullLogger) Error(args ...interface{}) {}
+func (nullLogger) Debug(msg string, args ...interface{}) {}
+func (nullLogger) Info(msg string, args ...interface{})  {}
+func (nullLogger) Warn(msg string, args ...interface{})  {}
+func (nullLogger) Error(msg string, args ...interface{}) {}
