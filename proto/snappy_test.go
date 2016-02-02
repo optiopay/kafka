f4758c02ff26b80605d870ebf1bd9ba9ed92ed1a
Mark Smith <mark@qq.is>
2016-02-02 11:40:50 -0800

    Switch to gocheck
    This library provides a simpler way of writing tests with more
    informative output. The various verbose/test selection mechanisms make
    quick iteration if tests easier.

--- proto/snappy_test.go
+++ proto/snappy_test.go
@@ -2,22 +2,27 @@ package proto
 
 import (
 	"bytes"
-	"testing"
+
+	. "gopkg.in/check.v1"
 )
+
+var _ = Suite(&SnappySuite{})
+
+type SnappySuite struct{}
 
 var snappyChunk = []byte("\x03\x08foo") // snappy encoding of "foo"
 
-func TestSnappyDecodeNormal(t *testing.T) {
+func (s *SnappySuite) TestSnappyDecodeNormal(c *C) {
 	got, err := snappyDecode(snappyChunk)
 	if err != nil {
-		t.Fatal(err)
+		c.Fatal(err)
 	}
 	if want := []byte("foo"); !bytes.Equal(got, want) {
-		t.Fatalf("got: %v; want: %v", got, want)
+		c.Fatalf("got: %v; want: %v", got, want)
 	}
 }
 
-func TestSnappyDecodeJava(t *testing.T) {
+func (s *SnappySuite) TestSnappyDecodeJava(c *C) {
 	javafied := []byte{
 		0x82, 'S', 'N', 'A', 'P', 'P', 'Y', 0x0, // magic
 		0, 0, 0, 1, // version
@@ -29,9 +34,9 @@ func TestSnappyDecodeJava(t *testing.T) {
 	}
 	got, err := snappyDecode(javafied)
 	if err != nil {
-		t.Fatal(err)
+		c.Fatal(err)
 	}
 	if want := []byte("foofoo"); !bytes.Equal(got, want) {
-		t.Fatalf("got: %v; want: %v", got, want)
+		c.Fatalf("got: %v; want: %v", got, want)
 	}
 }
